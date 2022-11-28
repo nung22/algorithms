712757e35c09a82fb48240755208cbdd4da45001
Nicholas Ung <ung.nicholask@gmail.com>
2022-11-28 08:18:37 -0800

    Initial Commit

---
+++ week_7/sll.js
@@ -0,0 +1,73 @@
+// MON
+
+/* 
+Intro to Singly Linked Lists
+    - OOP
+    - Array VS SLL
+
+*/
+
+
+class SLNode {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+    //  TUESDAY -------------------------------------------------------------------//
+    /**
+     * Prints all the nodes in the list to the console.
+     * of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {SinglyLinkedList} This list.
+     */
+    printValues() {
+
+    }
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBack(data) {
+
+    }
+
+    /**
+     * Adds each item of the given array to the back of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {Array<any>} vals The data for each new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    seedFromArr(vals) {
+
+    }
+
+}
+
+//==========================//
+//         TESTING          //
+//==========================//
+
+
+var sll = new SLL();
+sll.insertAtBack(1)
+sll.insertAtBack(2)
+sll.insertAtBack(3)
+sll.insertAtBack(4)
+sll.insertAtBack(5)
+
+sll.printValues()
+sll.insertAtFront(10)
+sll.removeAtFront()
+sll.printValues()
