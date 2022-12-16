cb21bb967ac8e60e3e210a1bbd86adf47f0cff91
Nicholas Ung <ung.nicholask@gmail.com>
2022-12-15 20:19:39 -0800

    Update

--- week_8/bst.js
+++ week_8/bst.js
@@ -298,6 +298,38 @@ class BinarySearchTree {
         }
         return false
     }
+        /**
+     * BFS order: horizontal rows top-down left-to-right.
+     * Converts this BST into an array following Breadth First Search order.
+     * Example on the fullTree var:
+     * [25, 15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90]
+     * @param {Node} current The current node during the traversal of this tree.
+     * @returns {Array<number>} The data of all nodes in BFS order.
+     */
+    toArrLevelOrder(current = this.root) {
+        const queue = []
+        const vals = []
+
+        if(node === null) {
+            return [];
+        }
+        // shift - it removes / returns the first element in the array
+        // const removedElement = queue.shift()
+    
+        // if current exists
+            // add the the curent node to the queue
+        
+        while(queue.length > 0){
+            // remove the first element in the queue
+            // PUSH TO vals THE BST NODE'S VALUE
+    
+            // IF REMOVED NODE'S LEFT EXISTS
+            // PUSH TO queue THE LEFT OF THE NODE
+            // IF REMOVED NODDE'S RIGHT EXISTS
+            // PUSH TO queue THE RIGHT OF THE NODE
+        }
+        // RETURN vals
+    }
 }
 
 // TEST CASES
