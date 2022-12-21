114aa0174232e9e72db28d604bdd3296cef08bae
Nicholas Ung <ung.nicholask@gmail.com>
2022-12-21 12:56:43 -0800

    Update

--- week_10/queue.js
+++ week_10/queue.js
@@ -1,4 +1,81 @@
 /**
+ * Class to represent a stack using an array to store the stacked items.
+ * Follows a LIFO (Last In First Out) order where new items are stacked on
+ * top (back of array) and removed items are removed from the top / back.
+ */
+class Stack {
+    /**
+     * The constructor is executed when instantiating a new Stack() to construct
+     * a new instance.
+     * @returns {Stack} This new Stack instance is returned without having to
+     *    explicitly write 'return' (implicit return).
+     */
+    constructor() {
+        this.items = [];
+    }
+    
+    /**
+     * Adds a new given item to the top / back of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to be added to the top / back.
+     * @returns {number} The new length of this stack.
+     */
+    push(item) {
+        this.items.push(item);
+        return this.items.length;
+    }
+    
+    /**
+     * Removes the top / last item from this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item or undefined if this stack was empty.
+     */
+    pop() {
+        return this.items.pop();
+    }
+    
+    /**
+     * Retrieves the top / last item from this stack without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The top / last item of this stack.
+     */
+    peek() {
+        if(this.isEmpty()) {
+            console.log("Stack is empty.");
+            return;
+        }
+        return this.items[this.size() - 1];
+    }
+    
+    /**
+     * Returns whether or not this stack is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.items.length > 0;
+    }
+    
+    /**
+     * Returns the size of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size() {
+        return this.items.length;
+    }
+
+    print() {
+        console.log(this.items);
+    }
+}
+
+/**
  * Class to represent a queue using an array to store the queued items.
  * Follows a FIFO (First In First Out) order where new items are added to the
  * back and items are removed from the front.
@@ -72,6 +149,34 @@ class Queue {
     print() {
         console.log(this.items);
     }
+
+    /**
+     * QUEUE MUST BE RETURNED TO IT'S ORIGINAL STATE
+     * You are only allowed to use built-in methods
+     * Use a stack to help solve
+     * 
+     * @returns {boolean} The queue is a palindrome
+     */
+    isPalindrome(){
+        let palindrome = true;
+        const tStack = new Stack();
+        for(let i=0; i<this.size(); i++) {
+            let temp = this.dequeue();
+            tStack.push(temp);
+            this.enqueue(temp);
+        }
+        // tStack.print();
+        // this.print();
+        for(let i=0; i<this.size(); i++) {
+            let temp = this.dequeue();
+            this.enqueue(temp);
+            if(temp !== tStack.pop()) {
+                palindrome = false;
+            }
+        }
+        return palindrome;
+    }
+
 }
 
 class QueueNode {
@@ -80,6 +185,39 @@ class QueueNode {
         this.next = null;
     }
 }
+
+
+let test = new Queue()
+test.enqueue('a')
+test.enqueue('b')
+test.enqueue('c')
+test.enqueue('b')
+test.enqueue('a')
+console.log(test.isPalindrome()) // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class LinkedQueue {
     constructor() {
@@ -131,25 +269,28 @@ class LinkedQueue {
 //!==========================//
 
 
-let queue1 = new Queue();
-console.log(queue1.isEmpty());
-console.log(queue1.front());
-console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
+// let queue1 = new Queue();
+// console.log(queue1.isEmpty());
+// console.log(queue1.front());
+// console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
 
-queue1.enqueue(25)
-queue1.enqueue(15)
-queue1.enqueue(10)
-queue1.enqueue(22)
-queue1.enqueue(4)
-queue1.enqueue(12)
-queue1.print();
-console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
+// queue1.enqueue(25)
+// queue1.enqueue(15)
+// queue1.enqueue(10)
+// queue1.enqueue(22)
+// queue1.enqueue(4)
+// queue1.enqueue(12)
+// queue1.print();
+// console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
 
-queue1.dequeue();
-queue1.dequeue();
-queue1.print();
-console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
+// queue1.dequeue();
+// queue1.dequeue();
+// queue1.print();
+// console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
 
-console.log(queue1.front());
-console.log(queue1.size());
-console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
+// console.log(queue1.front());
+// console.log(queue1.size());
+// console.log("~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~~>~>")
+
+
+
