ef1ba19957bd97cb9aae353cedaff48c9cf7b303
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-11 01:24:48 -0800

    Update insertionSort.js

--- week_11/insertionSort.js
+++ week_11/insertionSort.js
@@ -11,3 +11,31 @@ const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  * @param {Array<number>} nums
  * @returns {Array<number>} The given nums after being sorted.
  */
+
+const insertionSort = (nums) => {
+  // iterate through each element of the array nums
+  for(let i = 1; i < nums.length; i++) {
+    // sort if the element at 'i' is less than its left element
+    if(nums[i] < nums[i - 1]) {
+      // element to sort
+      let currElement = nums[i];
+      // final index of sorted element
+      let j = i;
+      // iterate through sorted portion of nums as long as the 
+      // current element is less than the element to its left
+      while(currElement < nums[j - 1]) {
+        // move left element forward one position
+        nums[j] = nums[j - 1];
+        // move index of current element back one position
+        j--;
+      }
+      // move current element to its correct position
+      nums[j] = currElement;
+    }
+  }
+  return nums;
+}
+
+console.log(insertionSort(numsOrdered));
+console.log(insertionSort(numsRandomOrder));
+console.log(insertionSort(numsReversed));
