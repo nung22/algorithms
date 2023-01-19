c4e97acb944f849364dd7a4ba51f8a0a5218d5ba
Nicholas Ung <ung.nicholask@gmail.com>
2023-01-19 09:06:46 -0800

    Create orderedMultiSetUnion.js

---
+++ week_12/orderedMultiSetUnion.js
@@ -0,0 +1,83 @@
+/* 
+  Union Sorted Arrays
+  Efficiently combine two already-sorted multiset arrays
+  into a new sorted array containing the multiset union.
+  Unions by default will take the set of dupes
+  that has the highest occurrences from one array.
+  Venn Diagram Visualization (top) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+
+const nums1A = [1, 2, 2, 2, 7];
+const nums1B = [2, 2, 6, 6, 7];
+const expected1 = [1, 2, 2, 2, 6, 6, 7];
+
+const nums2A = [1, 1, 2, 2, 2, 3, 7, 10, 20, 30];
+const nums2B = [2, 6, 6, 7];
+const expected2 = [1, 1, 2, 2, 2, 3, 6, 6, 7, 10, 20, 30];
+
+const nums3A = [];
+const nums3B = [2, 2, 3, 3, 3];
+const expected3 = [2, 2, 3, 3, 3];
+
+const nums4A = [2, 2, 3, 3, 3];
+const nums4B = [];
+const expected4 = [2, 2, 3, 3, 3];
+
+const nums5A = [];
+const nums5B = [];
+const expected5 = [];
+/* 
+  Explanation: Every int from each multiset is included in the result, for dupes, like 2, include it 3 times,
+  because it occurs 3 times at most in ONE multiset
+*/
+
+/**
+ * Combines two already sorted multiset arrays into an ordered multiset union
+ * Venn Diagram Visualization (top):
+ * @see https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} sortedA Both sets are sorted multisets
+ *    (contain dupes).
+ * @param {Array<number>} sortedB
+ * @returns {Array<number>} An ordered multiset union of the given sets.
+ *    The return should include dupes, but the amount of dupes for each int
+ *    should be based on the max amount that dupe appears from one set,
+ *    not the combined amount from both sets.
+ */
+function orderedMultisetUnion(sortedA, sortedB) {
+  // track current indices of sortedA & sortedB
+  let freqA = {} , freqB = {};
+  let result = {};
+  sortedA.forEach((item) => {
+    item in freqA
+      ? freqA[item] += 1
+      : freqA[item] = 1;
+  })
+  sortedB.forEach((item) => {
+    item in freqB
+      ? freqB[item] += 1
+      : freqB[item] = 1;
+  })
+  // console.log(freqA);
+  // console.log(freqB);
+  Object.keys(freqA).forEach((item) => {
+    console.log(item);
+    if(item in Object.keys(freqB)) {
+      
+    }
+  })
+  Object.keys(freqB).forEach((item) => {
+    console.log(item);
+  })
+  // Object.keys(freqA)
+  // Object.keys(freqB)
+
+  return result;
+}
+
+console.log(orderedMultisetUnion(nums1A, nums1B));
+console.log(orderedMultisetUnion(nums2A, nums2B));
+console.log(orderedMultisetUnion(nums3A, nums3B));
+console.log(orderedMultisetUnion(nums4A, nums4B));
+console.log(orderedMultisetUnion(nums5A, nums5B));
