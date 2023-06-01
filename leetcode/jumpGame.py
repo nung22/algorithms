a2686e93c315ac7b47dfe105234c0a670fad3c39
Nicholas Ung <37952714+nung22@users.noreply.github.com>
2023-06-01 15:40:26 -0700

    Create jumpGame.py

---
+++ leetcode/jumpGame.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/jump-game
+
+#* 55. Jump Game
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        j=len(nums)-1
+
+        while j>0:
+            if nums[j-1]>0:
+                j-=1
+            else:
+                i=j-1
+                while nums[i]<j-i:
+                    if i<=0:
+                        return False
+                    i-=1
+                j=i
+        return True
