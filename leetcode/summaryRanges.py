d43fd4937d72d66e8ea693a3318cc88186d349de
Nicholas Ung <ung.nicholask@gmail.com>
2023-06-08 17:27:51 -0700

    Create summaryRanges.py

---
+++ leetcode/summaryRanges.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/summary-ranges
+
+#* 228. Summary Ranges
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if len(nums) == 0: return []
+        if len(nums) == 1: return [f'{nums[0]}']
+        s = []
+        pre = start = nums[0]
+        for i in nums[1:]:
+            if i - pre != 1:
+                s.append(f'{start}->{pre}' if pre - start > 0 else f'{pre}')
+                start = i
+            pre = i
+        s.append(f'{start}->{pre}' if pre - start > 0 else f'{pre}')
+        return s
