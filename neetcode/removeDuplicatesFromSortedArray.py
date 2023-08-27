688fd5a41f1505485dbd6f239fcf504dbc0c4393
Nicholas Ung <ung.nicholask@gmail.com>
2023-08-26 23:52:20 -0700

    Update removeDuplicatesFromSortedArray.py

--- neetcode/removeDuplicatesFromSortedArray.py
+++ neetcode/removeDuplicatesFromSortedArray.py
@@ -1,20 +1,16 @@
-# https://leetcode.com/problems/3sum-closest
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array
 
-# * 16. 3Sum Closest
+# * 26. Remove Duplicates from Sorted Array
 
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        closet = float('inf')
-        nums.sort()
-        for i in range(len(nums) - 2):
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                sum3 = nums[i] + nums[l] + nums[r]
-                print(sum3)
-                if sum3 < target:
-                    l += 1
-                else:
-                    r -=1
-                if abs(sum3 - target) < abs(closet - target):
-                    closet = sum3
-        return closet
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i,j = 0,1
+        # iterate through nums array
+        while j < len(nums):
+            # increment j if there is a duplicated value
+            if nums[i] == nums[j]:
+                j += 1
+            else:
+                nums[i+1] = nums[j]
+                i += 1
+        return i + 1
