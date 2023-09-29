f40129fcddad5a69171c1d25902f3959500c0162
Nicholas Ung <nicholasung22@gmail.com>
2023-09-28 23:56:47 -0700

    Update minStack.py

--- neetcode/minStack.py
+++ neetcode/minStack.py
@@ -23,7 +23,6 @@ class MinStack:
     def getMin(self) -> int:
         return self.minStack[-1]
 
-
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
